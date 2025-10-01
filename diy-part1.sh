0d69bd99c68c11f6cc3e0669442e5dbe786c853f
Beanbag7 <56281910+Beanbag7@users.noreply.github.com>
2025-10-01 15:04:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git netkeeper https://github.com/CCnut/feed-netkeeper;LUCI-LUA-UCITRACK' >>feeds.conf.default
-echo 'src-git netkeeper https://github.com/RuijieNetworksCommunity/feed-netkeeper;LUCI-LUA-UCITRACK' >>feeds.conf.default
+echo 'src-git netkeeper https://github.com/RuijieNetworksCommunity/feed-netkeeper;master' >>feeds.conf.default
